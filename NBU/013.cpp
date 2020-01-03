74d776358bfb3ec9a13aa67e34845c656ef5a26b
Yilk <huangjk0311@126.com>
2020-01-03 21:44:34 +0800

    modify

--- NBU/013.cpp
+++ NBU/013.cpp
@@ -6,5 +6,6 @@ int main()
 	scanf("%lf %lf %lf %lf",&x1,&y1,&x2,&y2);//输入数据
 	s=sqrt((x2-x1)*(x2-x1)*+(y2-y1)*(y2-y1));//求距离
 	printf("%.2lf\n",s );
+	return 0;
 
 }
--- NBU/018.cpp
+++ NBU/018.cpp
@@ -3,7 +3,6 @@ int main()
 {
 	int m,n;
 	scanf("%d %d",&m,&n);
-	printf("%d ",m/n );
-	printf("%d ",m%n );
+	printf("%d %d\n",m/n,m%n );
 	return 0;
 }

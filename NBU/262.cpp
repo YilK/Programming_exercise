d8d8b319894c49de6fadff0575eb76e0a9737237
Yilk <huangjk0311@126.com>
2020-03-10 19:15:01 +0800

    0

---
+++ NBU/262.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num[21];
+	int a;
+	int i=0;
+	while(1)
+	{
+		cin>>a;
+		if(a>0)
+		{
+			num[i]=a;
+			i++;
+		}
+		else break;
+	}
+	//≈≈–ÚÀ„∑®
+	for(int j=0;j<i-1;j++)
+	{
+		int min=j;
+		for(int k=j+1;k<i;k++)
+		{
+			if(num[k]<num[min])
+				min=k;
+		}
+		if(min!=i)
+		{
+			int temp;
+			temp=num[j];
+			num[j]=num[min];
+			num[min]=temp;
+		}
+	} 
+	int flag=0;
+	for(int j=0;j<i;j++)
+	{
+		if(flag==num[j])
+			continue;
+		else
+		{
+			cout<<num[j]<<endl;
+			flag=num[j];
+		}
+	}
+	return 0;
+} 
